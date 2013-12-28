8f62c4b7352dd86cb208321f1b3bc30fbd9c7411
Eric Chapweske <eac@zendesk.com>
2013-12-27 16:40:10 -0800

    Oops, forgot this

---
+++ lib/preload_log.rb
@@ -0,0 +1,33 @@
+require 'active_record/assocations/preloader'
+
+module PredictiveLoad
+  class PreloadLog < ActiveRecord::Associations::Preloader
+
+    attr_accessor :logger
+
+    def preload(association)
+      grouped_records(association).each do |reflection, klasses|
+        klasses.each do |klass, records|
+          preloader   = preloader_for(reflection).new(klass, records, reflection, options)
+          preload_sql = preloader.scoped.where(collection_arel(preloader)).to_sql
+
+          log("would preload with: #{preload_sql.to_s}")
+          ActiveRecord::Base.connection.explain(preload_sql).each_line do |line|
+            log(line)
+          end
+        end
+      end
+    end
+
+    def collection_arel(preloader)
+      owners_map = preloader.owners_by_key
+      owner_keys = owners_map.keys.compact
+      preloader.association_key.in(owner_keys)
+    end
+
+    def log(message)
+      Rails.logger.info("lazy_loader: #{message}")
+    end
+
+  end
+end
